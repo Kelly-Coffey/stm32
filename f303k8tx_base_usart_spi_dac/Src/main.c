be5ea3c72038e036b029187cf17661cbc0023111
mick909 <mick.neumann.0313.1030@gmail.com>
2016-11-29 15:46:30 +0900

    modify usart2_dma config routine

--- f303k8tx_base_usart_spi_dac/Src/main.c
+++ f303k8tx_base_usart_spi_dac/Src/main.c
@@ -1,6 +1,9 @@
 #include "stm32f3xx_hal.h"
 #include "sysconf.h"
 
+void USART2_UART_DMA_Init(DMA_Channel_TypeDef *dmach);
+void UART2_Transmit_DMA(DMA_Channel_TypeDef *dmach, uint8_t *pData, uint16_t Size);
+
 int main(void)
 {
   SetSysClock_PLL_HSI();
@@ -11,7 +14,12 @@ int main(void)
   
   Init_Peripherals();
   
+  USART2_UART_DMA_Init(DMA1_Channel7);
+
   while (1) {
+    char buff[32];
+    UART2_Transmit_DMA(DMA1_Channel7, buff, 16);
+
     HAL_Delay(500);
   }
 }
