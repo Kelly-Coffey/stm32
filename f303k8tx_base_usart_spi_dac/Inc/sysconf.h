d451be99a4a9dc84d46461579a101b77c0650ad4
mick909 <mick.neumann.0313.1030@gmail.com>
2016-12-09 06:18:39 +0900

    cleanup

--- f303k8tx_base_usart_spi_dac/Inc/sysconf.h
+++
@@ -1,4 +0,0 @@
-void SetSysClock_PLL_HSI(void);
-void Init_NVIC();
-void Init_GPIO();
-void Init_Peripherals();
--- nucleo_f303k8_led/Src/_exit.c
+++
@@ -1,8 +0,0 @@
-void
-__attribute__((weak))
-_exit(int code __attribute__((unused)))
-{
-  while (1)
-  {}
-}
-
