07c6565ae3dd0745bbb91ee13860402d73cc1586
mick.909 <mickey@oomorikakanriyou-no-MacBook-Pro.local>
2017-01-07 13:22:00 +0900

    testing 10ms counter witn TIM2&TIM3

---
+++ timer_study/Src/main.c
@@ -0,0 +1,410 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f3xx_hal.h"
+#include "tim.h"
+#include "gpio.h"
+
+/* USER CODE BEGIN Includes */
+#include "usart2.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint8_t digdata[] = {0x5f, 0x41, 0x9d, 0xcd, 0xc3, 0xce, 0xde, 0x45, 0xdf, 0xcf};
+
+__IO uint16_t count = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+void _init(void)
+{
+}
+
+void Output_Digit(uint8_t dat, __IO uint32_t* brr)
+{
+  LED_DAT_GPIO_Port->BSRR = (LED_DAT_Pin << 16) | ((dat & 0x80) << 1);
+//  if (dat & 0x80) {
+//    LED_DAT_GPIO_Port->BSRR = LED_DAT_Pin;
+//  } else {
+//    LED_DAT_GPIO_Port->BRR = LED_DAT_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x40) << 2);
+//  if (dat & 0x40) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x20) << 3);
+//  if (dat & 0x20) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x10) << 4);
+//  if (dat & 0x10) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x08) << 5);
+//  if (dat & 0x08) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x04) << 6);
+//  if (dat & 0x04) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x02) << 7);
+//  if (dat & 0x02) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+
+/*  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin; */
+  LED_DAT_GPIO_Port->BSRR = ((LED_DAT_Pin | LED_SCK_Pin)<< 16) | ((dat & 0x01) << 8);
+//  if (dat & 0x01) {
+//    GPIOA->BSRR = LED_DAT_Pin | (LED_SCK_Pin << 16);
+//  } else {
+//    GPIOA->BRR = LED_DAT_Pin | LED_SCK_Pin;
+//  }
+  LED_SCK_GPIO_Port->BSRR = LED_SCK_Pin;
+  LED_SCK_GPIO_Port->BRR = LED_SCK_Pin;
+
+  *brr = LED_DAT_Pin;
+  LED_RCK_GPIO_Port->BSRR = LED_RCK_Pin;
+  LED_RCK_GPIO_Port->BRR = LED_RCK_Pin;
+}
+
+/* TIM2 : 100Hz Trigger Output from Internal Clock */
+/* OC1, OC2, OC3, OC4 : each 2.5ms outout compare */
+void TIM2_Init(void)
+{
+  __HAL_RCC_TIM2_CLK_ENABLE();
+
+  TIM2->CR1  = TIM_COUNTERMODE_UP | TIM_CLOCKDIVISION_DIV1;
+  TIM2->CR2  = TIM_TRGO_UPDATE;
+  TIM2->SMCR = 0;           /* ClockSource Internal */
+  TIM2->DIER = 0;           /* interrupt disable */
+
+  /* Frozen, Preload disabled */
+  TIM2->CCMR1 = 0;
+  TIM2->CCMR2 = 0;
+
+  TIM2->CCR1 = 1;
+  TIM2->CCR2 = 21;
+  TIM2->CCR3 = 41;
+  TIM2->CCR4 = 61;
+
+  /* CCxP = 0: active high, CCxE = 0: output compare enable */
+  TIM2->CCER = TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E;
+
+  TIM2->PSC = 1000-1;
+  TIM2->ARR = 80-1;
+  TIM2->EGR = TIM_EGR_UG;
+  do {} while ( !(TIM2->SR & TIM_SR_UIF) );
+
+  /* clear all flag */
+  TIM2->SR = 0;
+}
+
+/* TIM3 : salve counter (use TIM2 as a presacler) */
+void TIM3_Init(void)
+{
+  __HAL_RCC_TIM3_CLK_ENABLE();
+
+  TIM3->CR1  = TIM_COUNTERMODE_UP | TIM_CLOCKDIVISION_DIV1;
+  TIM3->CR2  = 0;
+  /* ClockSource External1, ITR1 (TIM2) */
+  TIM3->SMCR = TIM_SLAVEMODE_EXTERNAL1 | TIM_TS_ITR1;
+
+  TIM3->DIER = 0;           /* interrupt disable */
+
+  TIM3->PSC = 0;
+  TIM3->ARR = 60000-1;
+  TIM3->EGR = TIM_EGR_UG;
+  do {} while ( !(TIM3->SR & TIM_SR_UIF) );
+
+  /* clear all flag */
+  TIM3->SR = 0;
+}
+
+void init_start_led(void)
+{
+  Output_Digit(0x00, &LED_DAT_GPIO_Port->BSRR);
+  Output_Digit(0x00, &LED_DAT_GPIO_Port->BSRR);
+  Output_Digit(0x00, &LED_DAT_GPIO_Port->BSRR);
+  Output_Digit(0x00, &LED_DAT_GPIO_Port->BSRR);
+
+  /* Enable the Output compare channel */
+//  TIM_CCxChannelCmd(TIM2, TIM_CHANNEL_1, TIM_CCx_ENABLE);
+//  TIM_CCxChannelCmd(TIM2, TIM_CHANNEL_2, TIM_CCx_ENABLE);
+//  TIM_CCxChannelCmd(TIM2, TIM_CHANNEL_3, TIM_CCx_ENABLE);
+//  TIM_CCxChannelCmd(TIM2, TIM_CHANNEL_4, TIM_CCx_ENABLE);
+//  TIM2->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E;
+
+//  TIM2->EGR = TIM_EGR_UG;
+//  __HAL_TIM_CLEAR_FLAG(&htim2, TIM_FLAG_CC2);
+//  __HAL_TIM_CLEAR_FLAG(&htim2, TIM_FLAG_CC3);
+//  __HAL_TIM_CLEAR_FLAG(&htim2, TIM_FLAG_CC4);
+//  __HAL_TIM_CLEAR_FLAG(&htim2, TIM_FLAG_UPDATE);
+  // need some clock...
+//  do {} while ( !(TIM2->SR & TIM_SR_UIF) );
+//  TIM2->SR = ~(TIM_FLAG_CC2 | TIM_FLAG_CC3 | TIM_FLAG_CC4 | TIM_SR_UIF);
+
+//  TIM3->EGR = TIM_EGR_UG;
+//  __HAL_TIM_CLEAR_FLAG(&htim3, TIM_FLAG_UPDATE);
+//  (&htim3)->Instance->SR = ~(TIM_FLAG_UPDATE);
+  // need some clock...
+//  do {} while ( !(TIM3->SR & TIM_SR_UIF) );
+//  TIM3->SR = ~(TIM_SR_UIF);
+
+  /* Enable the Peripheral */
+//  __HAL_TIM_ENABLE(&htim3);
+//  __HAL_TIM_ENABLE(&htim2);
+  TIM3->CR1 |= TIM_CR1_CEN;
+  TIM2->CR1 |= TIM_CR1_CEN;
+}
+
+void led_drive(void)
+{
+  uint8_t dat;
+
+  if (TIM3->SR & TIM_SR_UIF) {
+    TIM3->SR = ~(TIM_SR_UIF);
+    TIM3->CNT = TIM3->CNT + 10000;
+  }
+
+  if (TIM2->SR & TIM_SR_CC1IF) {
+    TIM2->SR = ~(TIM_SR_CC1IF);
+    count = TIM3->CNT;
+    dat = digdata[count % 10];
+     Output_Digit(dat, &LED_DAT_GPIO_Port->BRR);
+  } else
+  if (TIM2->SR & TIM_SR_CC2IF) {
+    TIM2->SR = ~(TIM_SR_CC2IF);
+    dat = digdata[(count / 10) % 10];
+    Output_Digit(dat, &LED_DAT_GPIO_Port->BSRR);
+  } else
+  if (TIM2->SR & TIM_SR_CC3IF) {
+    TIM2->SR = ~(TIM_SR_CC3IF);
+    dat = digdata[(count / 100) % 10];
+    if ((count % 100) < 50) {
+      dat |= 0x20;
+    }
+    Output_Digit(dat, &LED_DAT_GPIO_Port->BSRR);
+  } else
+  if (TIM2->SR & TIM_SR_CC4IF) {
+    TIM2->SR = ~(TIM_SR_CC4IF);
+    if (count < 1000) {
+      dat = 0x00;
+    } else {
+      dat = digdata[(count / 1000) % 10];
+    }
+    Output_Digit(dat, &LED_DAT_GPIO_Port->BSRR);
+  }
+}
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+//  MX_TIM2_Init();
+//  MX_TIM3_Init();
+
+  /* USER CODE BEGIN 2 */
+  TIM2_Init();
+  TIM3_Init();
+
+  init_start_led();
+  count = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    led_drive();
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler */
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+*/
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ timer_study/Src/usart2.c
@@ -0,0 +1,180 @@
+
+#include "usart2.h"
+#include "gpio.h"
+
+/*!< UART or USART CR1 fields of parameters set by UART_SetConfig API */
+#define UART_CR1_FIELDS  ((uint32_t)(USART_CR1_M | USART_CR1_PCE | USART_CR1_PS | \
+                                     USART_CR1_TE | USART_CR1_RE | USART_CR1_OVER8))
+
+#define UART_BUFSIZE 128 /* 2^n (64,128,256,512...) */
+
+__IO uint8_t TX_Buff[UART_BUFSIZE];
+__IO unsigned int TX_head;
+__IO unsigned int TX_tail;
+__IO uint8_t RX_Buff[UART_BUFSIZE];
+__IO unsigned int RX_head;
+__IO unsigned int RX_tail;
+
+void USART2_UART_Init(void)
+{
+  UART2_Init();
+
+  /* Disable the Peripheral */
+  USART2->CR1 &= ~USART_CR1_UE;
+
+  /*-------------------------- USART CR1 Configuration -----------------------*/
+  /* Clear M, PCE, PS, TE, RE and OVER8 bits and configure
+   *  the UART Word Length, Parity, Mode and oversampling  */
+  MODIFY_REG(USART2->CR1, UART_CR1_FIELDS, UART_WORDLENGTH_8B | UART_PARITY_NONE | UART_MODE_TX_RX | UART_OVERSAMPLING_16);
+
+  /*-------------------------- USART CR2 Configuration -----------------------*/
+  /* Configure the UART Stop Bits: Set STOP[13:12] bits according
+   * to huart->Init.StopBits value */
+  MODIFY_REG(USART2->CR2, USART_CR2_STOP, UART_STOPBITS_1);
+
+  /*-------------------------- USART CR3 Configuration -----------------------*/
+  /* Configure
+   * - UART HardWare Flow Control: set CTSE and RTSE bits according
+   *   to huart->Init.HwFlowCtl value
+   * - one-bit sampling method versus three samples' majority rule according
+   *   to huart->Init.OneBitSampling */
+  MODIFY_REG(USART2->CR3, (USART_CR3_RTSE | USART_CR3_CTSE | USART_CR3_ONEBIT), UART_HWCONTROL_NONE | UART_ONE_BIT_SAMPLE_DISABLE);
+
+  USART2->BRR = (uint16_t)(UART_DIV_SAMPLING16(HAL_RCC_GetPCLK1Freq(), 38400));
+
+  /* In asynchronous mode, the following bits must be kept cleared:
+  - LINEN and CLKEN bits in the USART_CR2 register,
+  - SCEN, HDSEL and IREN  bits in the USART_CR3 register.*/
+  USART2->CR2 &= ~(USART_CR2_LINEN | USART_CR2_CLKEN);
+  USART2->CR3 &= ~(USART_CR3_SCEN | USART_CR3_HDSEL | USART_CR3_IREN);
+
+  /* Enable the Peripheral */
+  USART2->CR1 |=  USART_CR1_UE;
+
+  /* TEACK and REACK */
+  while ( !(USART2->ISR & USART_ISR_TEACK) );
+  while ( !(USART2->ISR & USART_ISR_REACK) );
+
+  TX_head = TX_tail = 0;
+  RX_head = RX_tail = 0;
+  
+  /* Enable Receive Data register not empty interrupt */
+  USART2->CR1 |= USART_CR1_RXNEIE;
+}
+
+void UART2_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* Peripheral clock enable */
+  __HAL_RCC_USART2_CLK_ENABLE();
+  
+  /**USART2 GPIO Configuration    
+    PA2     ------> USART2_TX
+    PA15     ------> USART2_RX 
+  */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* Peripheral interrupt init */
+  HAL_NVIC_SetPriority(USART2_IRQn, 4, 0);
+  NVIC_EnableIRQ(USART2_IRQn);
+}
+
+void UART2_DeInit(void)
+{
+  /* Peripheral clock disable */
+  __HAL_RCC_USART2_CLK_DISABLE();
+  
+  /* USART2 GPIO Configuration
+    PA2     ------> USART2_TX
+    PA15     ------> USART2_RX 
+  */
+  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_15);
+
+  /* Peripheral interrupt Deinit*/
+  NVIC_DisableIRQ(USART2_IRQn);
+}
+
+void UART2_Transmit(uint8_t c)
+{
+  unsigned int tmp_t = (TX_tail + 1) & (UART_BUFSIZE - 1);
+  
+  while (TX_head == tmp_t) ;
+
+  NVIC_DisableIRQ(USART2_IRQn);
+  TX_Buff[TX_tail] = c;
+  TX_tail = tmp_t;
+  NVIC_EnableIRQ(USART2_IRQn);
+
+  /* Enable Transmit Data Register empty interrupt */
+  USART2->CR1 |= USART_CR1_TXEIE;
+}
+
+void UART2_Transmit_String(const char *buff)
+{
+  while (*buff)
+  {
+    UART2_Transmit(*buff++);
+  }
+}
+
+uint8_t UART2_IsDataReceived(void)
+{
+  return (RX_head != RX_tail);
+}
+
+uint8_t UART2_Receive(void)
+{
+  uint8_t c;
+  unsigned int tmp_h = RX_head;
+
+  while (tmp_h == RX_tail) ;
+  
+  NVIC_DisableIRQ(USART2_IRQn);
+  c = RX_Buff[tmp_h++];
+  RX_head = tmp_h & (UART_BUFSIZE - 1);
+  NVIC_EnableIRQ(USART2_IRQn);
+  
+  return c;
+}
+
+/* handles USART2 global interrupt / USART2 wake-up interrupt through EXT line 26. */
+void USART2_IRQHandler(void)
+{
+  uint32_t isr = USART2->ISR;
+
+  if (isr & USART_ISR_RXNE)
+  {
+    unsigned int tmp_t = (RX_tail + 1) & (UART_BUFSIZE-1);
+    unsigned int tmp_h = RX_head;
+
+    if (tmp_t != tmp_h) {
+      RX_Buff[RX_tail] = USART2->RDR;
+      RX_tail = tmp_t;
+    } else {
+      /* overflow */
+    }
+  }
+
+  if (isr & USART_ISR_TXE)
+  {
+    unsigned int tmp_t = TX_tail;
+    unsigned int tmp_h = TX_head;
+
+    if (tmp_t == tmp_h) {
+      /* Transfer completed. */
+      /* Disable Transmit Data Register empty interrupt */
+      USART2->CR1 &= ~USART_CR1_TXEIE;
+    } else {
+      USART2->TDR = TX_Buff[tmp_h];
+      TX_head = (tmp_h + 1) & (UART_BUFSIZE - 1);
+    }
+  }
+}
+
+
