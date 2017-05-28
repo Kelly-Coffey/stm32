48f80bcb0f2f7b9fe37f3dffb382f2ba81a61f7f
mick.909 <mickey@oomorikakanriyou-no-MacBook-Pro.local>
2017-05-28 17:41:07 +0900

    modify project setting

--- stm32l151_timer_rtos/Inc/stm32l1xx_hal_conf.h
+++ stm32l151_timer_rtos/Inc/stm32l1xx_hal_conf.h
@@ -133,9 +133,9 @@
 #define  VDD_VALUE					  ((uint32_t)3300) /*!< Value of VDD in mv */           
 #define  TICK_INT_PRIORITY            ((uint32_t)15)    /*!< tick interrupt priority */            
 #define  USE_RTOS                     0     
-#define  PREFETCH_ENABLE              0
-#define  INSTRUCTION_CACHE_ENABLE     1
-#define  DATA_CACHE_ENABLE            1
+#define  PREFETCH_ENABLE              1
+#define  INSTRUCTION_CACHE_ENABLE     0
+#define  DATA_CACHE_ENABLE            0
 
 /* ########################## Assert Selection ############################## */
 /**
